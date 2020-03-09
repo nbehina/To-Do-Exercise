1a6f955ff97ad57b32216cc15f1cc0296f4b89fc
Your Namenbehina <nbehina@gmail.com>
2020-03-08 21:14:14 -0600

    To-Do Exercise-Second Version

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,1 +1,22 @@
-var myList = document.getElementById( 'enter-task' );
+var form = document.getElementById( 'my-form' );
+var list = document.getElementById('my-list');
+
+form.addEventListener( 'submit', function( event ) {
+event.preventDefault();
+
+var item = document.getElementById('item').value;
+
+var newRow = document.createElement( 'ul' );
+
+var newColName = document.createElement( 'li' );
+    newColName.innerHTML = Trash;
+    newRow.appendChild( newColName );
+    
+var newColName = document.createElement( 'li' );
+newColName.innerHTML = Milk;
+newRow.appendChild( newColName );
+
+
+document.getElementById( 'item' ).value = '';
+
+        });
